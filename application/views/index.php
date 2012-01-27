176074b04804de8c47f4f0441d9f316371866fc6
FLAq praca <flaqmastahgf@gmail.com>
2012-01-27 14:38:19 +0100

    uklad w css

--- application/views/index.php
+++ application/views/index.php
@@ -1,11 +1,48 @@
-
+﻿
+<body>
 <body>
 
-<div class="transbox">
-<div class="banner">
-<img src="<?php echo base_url()?>images/baner.jpg">
-</div>
-</div>
+    <div id="transbox">
+	
+	<h1 id="header">Nagłówek strony</h1>
+        
+        <div id="wrapper">
+        
+    <ul id="menu">
+        <li><a href="link1.html">Link1</a></li>
+        <li><a href="link2.html">Link2</a></li>
+        <li><a href="link3.html">Link3</a></li>
+    </ul>
+            
+        <div id="content">
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+            <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna. Ut interdum ligula sed tellus. Vivamus accumsan, dui eu consequat molestie, erat nisi bibendum velit, id vulputate dui risus quis ante. </p>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+            <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna. Ut interdum ligula sed tellus. Vivamus accumsan, dui eu consequat molestie, erat nisi bibendum velit, id vulputate dui risus quis ante. Proin at enim pulvinar odio pulvinar mattis. Sed ut diam in ipsum tempor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+            <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna. Ut interdum ligula sed tellus. Vivamus accumsan, dui eu consequat molestie, erat nisi bibendum velit, id vulputate dui risus quis ante. </p>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+            <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna. Ut interdum ligula sed tellus. Vivamus accumsan, dui eu consequat molestie, erat nisi bibendum velit, id vulputate dui risus quis ante. </p>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+            <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna. Ut interdum ligula sed tellus. Vivamus accumsan, dui eu consequat molestie, erat nisi bibendum velit, id vulputate dui risus quis ante. Proin at enim pulvinar odio pulvinar mattis. Sed ut diam in ipsum tempor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+            <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna. Ut interdum ligula sed tellus. Vivamus accumsan, dui eu consequat molestie, erat nisi bibendum velit, id vulputate dui risus quis ante. </p>
+        </div>     
+     </div>
+    
+    <div id="sidebar">
+        <h3>Wielka afera W3C</h3>
+           <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna.</p>
+        <h3>Google kupi�o Microsoft</h3>
+           <p>Vestibulum sed diam. Aliquam ut massa in ante pellentesque viverra. Phasellus rhoncus blandit urna.</p>    
+    </div>
+
+
+    <div id="footer">Copyright &copy; 2009 by wile kwak</div>
+	</div>
+</body>
+
+
 
 </body>
 </html>
--- application/views/partial/head.php
+++ application/views/partial/head.php
@@ -1,10 +1,18 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" >
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
 <title>Techplus | <?php echo($title)?> </title>
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="description" content="Your website description goes here" />
 <meta name="keywords" content="your,keywords,goes,here" />
 <link rel="stylesheet" type="text/css" href="<?php echo(base_url());?>css/style.css" />
+<script type="text/javascript" src="<?php echo(base_url());?>js/minmax.js"></script>
 </head>
+
+
+<head>
+
+
+    
+   
+  
