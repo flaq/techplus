deb7f9ace2f09a7a937478e51941d0338f12ed0f
FLAq <flaqmastahgf@gmail.com>
2012-01-28 16:57:20 +0100

    dzialajace linki

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -11,12 +11,55 @@ function index()
    $this->load->view('partial/transbox_top');
    $this->load->view('partial/header');
    $this->load->view('partial/menu');
-   $this->load->view('partial/content');
+   $this->load->view('partial/about');
    $this->load->view('partial/transbox_bottom');  
    $this->load->view('partial/sidebar');
    $this->load->view('partial/footer');
   
  
    }
+
+function view($page = 'about')
+   {
+   $data = array(
+		'title' => $page
+		);
+	
+	if ( ! file_exists('application/views/partial/'.$page.'.php'))
+	{
+		// Whoops, we don't have a page for that!
+		show_404();
+	}
+  
+   $this->load->view('partial/head', $data);
+   $this->load->view('partial/transbox_top');
+   $this->load->view('partial/header');
+   $this->load->view('partial/menu');
+   $this->load->view('partial/'.$page);
+   $this->load->view('partial/transbox_bottom');  
+   $this->load->view('partial/sidebar');
+   $this->load->view('partial/footer');
+  
+ 
+   }   
+function budowa()
+   {
+   $data = array(
+		'title' => 'Budowa domów'
+		);
+  
+   $this->load->view('partial/head', $data);
+   $this->load->view('partial/transbox_top');
+   $this->load->view('partial/header');
+   $this->load->view('partial/menu');
+   $this->load->view('partial/budowa');
+   $this->load->view('partial/transbox_bottom');  
+   $this->load->view('partial/sidebar');
+   $this->load->view('partial/footer');
+  
+ 
+   }      
+  
+  
 }
  ?>
